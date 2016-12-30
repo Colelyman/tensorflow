a8848bbcbc67848e1b88a078ed91b9f5668b59ed
Cole Lyman <cole@colelyman.com>
2016-12-30 13:33:36 -0700

    Created file to follow CNN tutorial

---
+++ multi-conv-nn/NLP-CNN.py
@@ -0,0 +1,7 @@
+'''
+@author Cole Lyman
+@desc A Convolutional neural network following the tutorial at
+http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/
+'''
+
+
